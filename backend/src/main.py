a7c2cee45307ed205419541c477301fba273fd68
Lucia Canzian <3003493@stud.hs-mannheim.de>
2025-05-05 13:57:06 +0200

    ES LÄUFT

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from src.api.routes.KIImage_routes import router
+#from src.api.routes.KIImage_routes import router
+from src.api.routes import KIContainer_routes,KIImage_routes 
 
 app = FastAPI()
 
@@ -8,4 +9,5 @@ def root():
     return {"message": "Welcome"}
 
 
-app.include_router(router)
+app.include_router(KIImage_routes.router)
+app.include_router(KIContainer_routes.router)
