e3d29825303af0bf894d231f228e4c0a809c91cb
Lucia Canzian <3003493@stud.hs-mannheim.de>
2025-05-06 13:21:13 +0200

    Ein import auskommentiert

--- backend/src/api/routes/KIImage_routes.py
+++ backend/src/api/routes/KIImage_routes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, HTTPException, UploadFile, File, Form, Depends
 from sqlalchemy.orm import Session
 from src.api.py_models import *
-from src.services.image_upload.service_KIImage import *
+#from src.services.image_upload.service_KIImage import *
 from src.db.database.database import get_db
 from src.db.crud import crud
 import logging
