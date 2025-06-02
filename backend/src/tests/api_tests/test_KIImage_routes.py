3faa2c5a4fa088e85dd8511429ab97d1192777dd
Lucia Canzian <3003493@stud.hs-mannheim.de>
2025-06-02 20:43:20 +0200

    Tests geaddet, Klare Struktur

--- backend/src/tests/api_tests/test_KIImage_routes.py
+++ backend/src/tests/api_tests/test_KIImage_routes.py
@@ -8,7 +8,11 @@ client = TestClient(app)
 
 app.dependency_overrides[get_db] = override_get_db
 
-def test_get_ki_images():
+# ========================================
+# KI-Image Routen: 2x GET, 2x POST, 1x DELETE, 1x PATCH
+# ========================================
+
+def test_get_list_of_ki_images():
     response = client.get("/ki-images")
     assert response.status_code == 200
     assert len(response.json()) == 2
@@ -31,5 +35,27 @@ def test_post_ki_image_from_hub():
     assert data["image_name"] == "hello-world"
     assert data["image_tag"] == "latest"
     assert "image_created_at" in data
+    num_image = client.get("/ki-images")
+    assert len(num_image.json()) == 3
+
+def test_delete_ki_image():
+    response = client.delete("/ki-images/300")
+    assert response.status_code == 200
+    data = response.json()
+    num_image = client.get("/ki-images")
+    assert len(num_image.json()) == 2
+
+def test_delete_ki_image_by_wrong_id():
+    response = client.delete("/ki-images/3")
+    assert response.status_code == 404
+
+# ========================================
+# KI-Container Routen: 1x GET, 2x POST, 1x DELETE
+# ========================================
+
+# ========================================
+# DICOM Routen: 2x GET, 1x POST, 2x DELETE
+# ========================================
+
 
 
